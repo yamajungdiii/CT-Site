a2ddd7107d5fbf0d9a5407d5c0e1a7d53b206c96
yamajungdiii <masterega47@gmail.com>
2026-02-26 16:01:38 +0700

    Clear

--- wp-content/plugins/elementor/assets/js/packages/editor-ui/editor-ui.strings.js
+++
@@ -1,8 +0,0 @@
-__( 'Clear', 'elementor' );
-__( "Don't show this again", 'elementor' );
-__( 'Got it introduction', 'elementor' );
-__( 'Got it', 'elementor' );
-__( 'Upgrade Now', 'elementor' );
-__( 'Not now', 'elementor' );
-__( 'Delete', 'elementor' );
-__( 'Upgrade now', 'elementor' );
